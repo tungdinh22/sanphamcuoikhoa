9a20c710148037f28924a67507a58683e47484b8
tungdinh22 <buidinhtung952@gmail.com>
2025-03-02 11:32:07 +0700

    firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,4 @@
- type="module"
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
